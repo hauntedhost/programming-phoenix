1987c24be27d022ea6b4a4af8c1f04068524c496
Sean Omlor <somlor@eml.cc>
2015-10-04 19:58:36 -0400

    new rumbl app

---
+++ rumbl/lib/rumbl.ex
@@ -0,0 +1,30 @@
+defmodule Rumbl do
+  use Application
+
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Start the endpoint when the application starts
+      supervisor(Rumbl.Endpoint, []),
+      # Start the Ecto repository
+      worker(Rumbl.Repo, []),
+      # Here you could define other workers and supervisors as children
+      # worker(Rumbl.Worker, [arg1, arg2, arg3]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Rumbl.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    Rumbl.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
