2a7d84d306c1f572317a5e2654fef588578ac020
Sean Omlor <somlor@eml.cc>
2016-02-28 23:02:44 -0500

    in memory rumbl repo

--- rumbl/lib/rumbl.ex
+++ rumbl/lib/rumbl.ex
@@ -10,7 +10,7 @@ defmodule Rumbl do
       # Start the endpoint when the application starts
       supervisor(Rumbl.Endpoint, []),
       # Start the Ecto repository
-      supervisor(Rumbl.Repo, []),
+      # supervisor(Rumbl.Repo, []),
       # Here you could define other workers and supervisors as children
       # worker(Rumbl.Worker, [arg1, arg2, arg3]),
     ]
