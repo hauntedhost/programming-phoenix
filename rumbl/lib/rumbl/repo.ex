fb8838df58982f90c0d5a3dd83625bf37b6d583d
Sean Omlor <somlor@eml.cc>
2016-03-04 18:46:48 -0500

    repo get_first/last helpers

--- rumbl/lib/rumbl/repo.ex
+++ rumbl/lib/rumbl/repo.ex
@@ -1,5 +1,18 @@
 defmodule Rumbl.Repo do
   use Ecto.Repo, otp_app: :rumbl
+  import Ecto.Query, only: [from: 2, order_by: 3]
+
+  def get_first(query) do
+    one from r in query,
+      order_by: [asc: r.inserted_at],
+      limit: 1
+  end
+
+  def get_last(query) do
+    one from r in query,
+      order_by: [desc: r.inserted_at],
+      limit: 1
+  end
 
   def get_by_uuid!(model, id) do
     case Ecto.Type.dump(Ecto.UUID, id) do
